9be1afe199f4d17f077688a5e5270cb7b85b00e8
Aji Prio Sejati <ajiprio@hotmail.com>
2023-10-22 02:21:37 +0700

    Add: post details routes

--- src/routes/post/index.tsx
+++ src/routes/post/index.tsx
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet"
 import { useParams } from "react-router-dom"
 
 export default function Post() {
   let {postId} = useParams()
   console.log(postId)
   return (
+    <>
+      <Helmet>
+        <title>Post {postId} - hello-world</title>
+      </Helmet>
     <div>Post {postId}</div>
+    </>
   )
 }
